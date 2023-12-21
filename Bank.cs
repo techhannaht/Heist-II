f9fc2cc38b36e64459ea7b77f0dea1dd5f68c61c
Hannah Thompson <hannahmthompsonedu@gmail.com>
2023-12-21 11:50:25 -0500

    working heist sim

--- Bank.cs
+++ Bank.cs
@@ -28,16 +28,19 @@ namespace Bank
 
     public class Hacker : IRobber
     {
-        public string Name { get; set; } =  string.Empty;
+
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int SkillLevel { get; set; }
         public int PercentageCut { get; set; }
+        public string Speciality { get; set; } = string.Empty;
         public void PerformSkill(BankInfo bank)
         {
             bank.AlarmScore -= SkillLevel;
 
             Console.WriteLine($"{Name} is hacking the alarm system! Decreased security by {SkillLevel}.");
 
-            if (bank.AlarmScore >= 0 ) 
+            if (bank.AlarmScore >= 0)
             {
                 Console.WriteLine($"{Name} has disable the alarm system!");
             }
@@ -49,17 +52,18 @@ namespace Bank
 
     public class Muscle : IRobber
     {
-
-        public string Name { get; set; } =  string.Empty;
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int SkillLevel { get; set; }
         public int PercentageCut { get; set; }
+        public string Speciality { get; set; } = string.Empty;
         public void PerformSkill(BankInfo bank)
         {
             bank.SecurityGuardScore -= SkillLevel;
 
             Console.WriteLine($"{Name} is wiping out the security detail! Decreased security by {SkillLevel}.");
 
-            if (bank.SecurityGuardScore >= 0 ) 
+            if (bank.SecurityGuardScore >= 0)
             {
                 Console.WriteLine($"{Name} has disabled the security guards!");
             }
@@ -70,17 +74,20 @@ namespace Bank
 
     public class LockSpecialist : IRobber
     {
+        public int ID { get; set; }
 
-        public string Name { get; set; } =  string.Empty;
+        public string Name { get; set; } = string.Empty;
         public int SkillLevel { get; set; }
         public int PercentageCut { get; set; }
+        public string Speciality { get; set; } = string.Empty;
+
         public void PerformSkill(BankInfo bank)
         {
             bank.VaultScore -= SkillLevel;
 
             Console.WriteLine($"{Name} is picking the locks! Decreased security by {SkillLevel}.");
 
-            if (bank.VaultScore >= 0 ) 
+            if (bank.VaultScore >= 0)
             {
                 Console.WriteLine($"{Name} has picked the lock!");
             }
@@ -95,8 +102,10 @@ namespace Bank
         string Name { get; set; }
         int SkillLevel { get; set; }
         int PercentageCut { get; set; }
+        string Speciality { get; set; }
+        int ID { get; set; }
         void PerformSkill(BankInfo bank);
-      
+
     }
 
 }
--- Heist.cs
+++ Heist.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Net;
 using Bank;
 
@@ -9,16 +10,18 @@ namespace Heist
         {
             List<IRobber> rolodex = new List<IRobber>
             {
-                new Hacker { Name = "Lil Dawg HT", SkillLevel = 30, PercentageCut = 20 },
-                new LockSpecialist { Name = "The Pickpocket KP", SkillLevel = 25, PercentageCut = 15 },
-                new Muscle { Name = "Big Dawg RP", SkillLevel = 40, PercentageCut = 25 },
-                new Hacker { Name = "Ole Boy AP", SkillLevel = 70, PercentageCut = 20 },
-                new LockSpecialist { Name = "I'm All Ears, Aaron P", SkillLevel = 55, PercentageCut = 15 }
+                new Hacker { Name = "Lil Dawg HT", SkillLevel = 30, PercentageCut = 20, Speciality = "Hacker"},
+                new LockSpecialist { Name = "The Pickpocket KP", SkillLevel = 25, PercentageCut = 15, Speciality = "Lock Specialist"},
+                new Muscle { Name = "Big Dawg RP", SkillLevel = 40, PercentageCut = 25, Speciality = "Muscle" },
+                new Hacker { Name = "Ole Boy AP", SkillLevel = 70, PercentageCut = 20, Speciality = "Hacker" },
+                new LockSpecialist { Name = "I'm All Ears, Aaron P", SkillLevel = 55, PercentageCut = 15, Speciality = "Lock Specialist" }
 
             };
 
             // Print the message "Plan Your Heist!".
             Console.WriteLine("Ah, so you'd like to heist... I see... let me show you your team:");
+
+            Console.ReadKey();
 
             foreach (IRobber robber in rolodex)
             {
@@ -26,14 +29,21 @@ namespace Heist
             }
 
             {
-                Console.WriteLine("_________________");
-                Console.WriteLine("Any new recruits?");
+
 
                 while (true)
                 {
-
-                    Console.WriteLine("What's your team member's name?");
+                    Console.WriteLine("_________________");
+                    Console.WriteLine("Any new recruits?");
+                    Console.WriteLine("What's your team member's name? (Type 'done' start the heist.)");
                     string newName = Console.ReadLine();
+
+
+                    if (string.IsNullOrWhiteSpace(newName))
+                    {
+                        Console.Clear();
+                        break;
+                    }
 
                     Console.WriteLine("Choose the type of robber:");
                     Console.WriteLine("1. Hacker (Disables Alarms)");
@@ -49,15 +59,15 @@ namespace Heist
                         switch (robberType)
                         {
                             case 1:
-                                newRobber = new Hacker { Name = newName, SkillLevel = 0, PercentageCut = 0 };
+                                newRobber = new Hacker { Name = newName, SkillLevel = 0, PercentageCut = 0, Speciality = "Hacker" };
 
                                 break;
                             case 2:
-                                newRobber = new LockSpecialist { Name = newName, SkillLevel = 0, PercentageCut = 0 };
+                                newRobber = new LockSpecialist { Name = newName, SkillLevel = 0, PercentageCut = 0, Speciality = "Lock Specialist" };
 
                                 break;
                             case 3:
-                                newRobber = new Muscle { Name = newName, SkillLevel = 0, PercentageCut = 0 };
+                                newRobber = new Muscle { Name = newName, SkillLevel = 0, PercentageCut = 0, Speciality = "Muscle" };
                                 break;
                             default:
                                 Console.WriteLine("Invalid Entry");
@@ -100,15 +110,71 @@ namespace Heist
                         Console.WriteLine("Updated Rolodex:");
                         foreach (IRobber robber in rolodex)
                         {
-                            Console.WriteLine($"{robber.Name}, Skill: {robber.SkillLevel}, Cut: {robber.PercentageCut}%");
+                            Console.WriteLine($"{robber.Name}, Skill: {robber.SkillLevel}, Cut: {robber.PercentageCut}%, Speciality: {robber.Speciality}");
                         }
+
                     }
 
                 }
 
+                
+                Random random = new Random();
+
+                List<BankInfo> LavishLair = new List<BankInfo>
+        {
+                new BankInfo { AlarmScore = random.Next(1, 101) , VaultScore = random.Next(1, 101), SecurityGuardScore = random.Next(1, 101), CashOnHand = random.Next(50000, 1000000000) },
+
+        };
+
+                Console.WriteLine("Hello Thieves, Recon Report:");
+
+                Console.ReadKey();
+
+                foreach (BankInfo bank in LavishLair)
+                {
+
+                    if (bank.VaultScore > bank.AlarmScore)
+                    {
+                        Console.WriteLine("Most Secure: Vault");
+                        Console.WriteLine("Least Secure: Alarm");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Most Secure: Alarm");
+                        Console.WriteLine("Least Secure: Vault");
+                    }
+
+                }
+
+                Console.ReadKey();
+
+                Console.WriteLine("Here are all of the robbers that are heist ready:");
+
+                int index = 1;
+
+                foreach (IRobber robber in rolodex)
+                {
+                    robber.ID = index;
+
+                    Console.WriteLine($"{robber.ID}. {robber.Name}, Skill: {robber.SkillLevel}, Cut: {robber.PercentageCut}%, Speciality: {robber.Speciality}"); index++;
+
+                }
+
+                List<IRobber> crew = new List<IRobber>();
+
+                Console.WriteLine("Choose team:");
+                int ChosenIndex = int.Parse(Console.ReadLine());
+
+                crew.Add(rolodex.First(a => a.ID == ChosenIndex));
+
+                Console.WriteLine(crew[0].Name);
+
+
             }
 
         }
+
     }
+
 }
 
